504360ac81c8a4875e333dd92c18f2b028f86201
poorn <poorn@DESKTOP-VN4OF00>
2022-10-16 12:16:32 +0530

    jwtdependency

---
+++ src/main/java/com/group7/lms/utility/JwtUtility.java
@@ -0,0 +1,24 @@
+package com.group7.lms.utility;
+
+
+
+import io.jsonwebtoken.Claims;
+
+//@Component
+public class JwtUtility {
+
+	 	private static final String SECRET_KEY = "lms_lms_lms";
+
+	    private static final int TOKEN_VALIDITY = 3600 * 5;
+	    
+	    
+	   
+
+		private String getClaimFromToken(String token, Object object) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+	
+	
+}
