3b8ecd5f761a17d4784d32c55921d3cd6168153d
poorn <poorn@DESKTOP-VN4OF00>
2022-10-16 15:26:27 +0530

    passwordencoder

--- src/main/java/com/group7/lms/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/group7/lms/security/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,17 @@
 package com.group7.lms.security;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+
+
+
 public class JwtAuthenticationEntryPoint {
+	
+	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException, java.io.IOException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
 
 }
--- src/main/java/com/group7/lms/security/PasswordEncoder.java
+++ src/main/java/com/group7/lms/security/PasswordEncoder.java
@@ -1,5 +1,11 @@
 package com.group7.lms.security;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class PasswordEncoder {
 
+	
+	public BCryptPasswordEncoder bCryptPasswordEncoder(){
+	    return new BCryptPasswordEncoder();
+	}
 }
